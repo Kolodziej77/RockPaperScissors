14cf77ca404708f86dee6ccf8aca193615a9c9bb
Damian <206709858+Kolodziej77@users.noreply.github.com>
2025-08-30 22:12:44 +0200

    Update game with UI integration and dynamic score tracking
    - Updated oneRound() to displau choices in UI
    - Added visual feedback for win/loss/tie using color styling
    - Disable buttons after game ends to prevent further input
    - Reset UI elements when starting a new game

--- script.js
+++ script.js
@@ -11,9 +11,13 @@ function computerChoice(){
 function oneRound(yourChoice, computerChoice){
     console.log('You chose: ' + yourChoice);
     console.log('Computer chose: ' + computerChoice);
+    userHeader.textContent = 'You chose: ' + `${yourChoice}`;
+    computerHeader.textContent = 'Computer chose: ' + `${computerChoice}`;
 
     if(yourChoice === computerChoice){
         console.log('It\'s a tie');
+        changeResult.textContent = 'It\'s a tie';
+        changeResult.style.color = 'black';
         return 0;
     }
 
@@ -21,25 +25,31 @@ function oneRound(yourChoice, computerChoice){
         case 'rock':
             if(computerChoice === 'paper'){
                 console.log('You lost this round');
+                loseStyle('You lost this round');
                 return -1;
             }
             console.log('You win this round');
+            winStyle('You win this round');
             return 1;
         
         case 'paper':
             if(computerChoice === 'scissors'){
                 console.log('You lost this round');
+                loseStyle('You lost this round');
                 return -1;
             }
             console.log('You win this round');
+            winStyle('You win this round');
             return 1;
 
         case 'scissors':
             if(computerChoice === 'rock'){
                 console.log('You lost this round');
+                loseStyle('You lost this round');
                 return -1;
             }
             console.log('You win this round');
+            winStyle('You win this round');
             return 1;
     }
 }
@@ -61,37 +71,72 @@ function playGame(){
     }
 
     return function(yourChoice){
-        if(currentRound >= rounds){
-            console.log('------- Final result -------');
-            if(yourScore > computerScore){
-                console.log('You won');
-            }else if(yourScore < computerScore){
-                console.log('You lost');
-            }
-            console.log('It\'s a tie');
-            return;
-        }
 
+        roundHeader.textContent = 'Round ' + `${currentRound + 1}`;
         console.log('------- Round ' + (currentRound + 1) + ' -------');
         let computerSelection = computerChoice();
         const roundResult = oneRound(yourChoice, computerSelection);
         if(roundResult === 1){
             yourScore++;
+            userPoints.textContent = yourScore;
         }else if(roundResult === -1){
             computerScore++;
+            computerPoints.textContent = computerScore;
         }
         console.log('You: ' + yourScore + ' | Computer: ' + computerScore);
         currentRound++;
+
+        if(currentRound >= rounds){
+            console.log('------- Final result -------');
+            if(yourScore > computerScore){
+                console.log('You won');
+                winStyle('You won');
+            }else if(yourScore < computerScore){
+                console.log('You lost');
+                winStyle('You lost');
+            }else{
+                console.log('It\'s a tie');
+                winStyle('It\'s a tie');
+            }
+
+            buttons.forEach(button => {
+                button.disabled = true;
+            })
+            return;
+        }        
     }   
+}
+
+function winStyle(str){
+    changeResult.textContent = `${str}`;
+    changeResult.style.color = 'green';
+}
+
+function loseStyle(){
+    changeResult.textContent = 'You lost this round';
+    changeResult.style.color = 'red';  
 }
 
 const buttons = document.querySelectorAll('#rock, #paper, #scissors');
 const newGameBtn = document.querySelector('#newGameBtn');
 const infoHeader = document.querySelector('#infoHeader');
+const changeResult = document.querySelector('#changeResult');
+const userPoints = document.querySelector('#userPoints');
+const computerPoints = document.querySelector('#computerPoints');
+const userHeader = document.querySelector('#userChoice');
+const computerHeader = document.querySelector('#computerChoice');
+const roundHeader = document.querySelector('#rounds');
 let playRound;
 
 newGameBtn.addEventListener('click', () => {
-    infoHeader.textContent = 'Choose Rock, Paper or Scissors';
+    infoHeader.textContent = 'Choose Rock, Paper, or Scissors';
+    userPoints.textContent = 0;
+    computerPoints.textContent = 0;
+    changeResult.textContent = '';
+
+    buttons.forEach(button => {
+        button.disabled = false;
+    })    
     playRound = playGame();
 });
 

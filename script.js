678909c7d78b19605f1703ec8f4ba8d44d5655d8
Damian <206709858+Kolodziej77@users.noreply.github.com>
2025-08-30 17:42:34 +0200

    Add gameplay logic with rounds and score tracking
    Implemented oneRound and playGame functions to handle game rounds, scoring and displaying result

--- script.js
+++ script.js
@@ -19,3 +19,80 @@ function yourChoice(){
     }
 }
 
+function oneRound(yourChoice, computerChoice){
+    console.log('You chose: ' + yourChoice);
+    console.log('Computer chose: ' + computerChoice);
+
+    if(yourChoice === computerChoice){
+        console.log('It\'s a tie');
+        return 0;
+    }
+
+    switch(yourChoice){
+        case 'rock':
+            if(computerChoice === 'paper'){
+                console.log('You lost this round');
+                return -1;
+            }
+            console.log('You win this round');
+            return 1;
+        
+        case 'paper':
+            if(computerChoice === 'scissors'){
+                console.log('You lost this round');
+                return -1;
+            }
+            console.log('You win this round');
+            return 1;
+
+        case 'scissors':
+            if(computerChoice === 'rock'){
+                console.log('You lost this round');
+                return -1;
+            }
+            console.log('You win this round');
+            return 1;
+    }
+}
+
+function playGame(){
+    let yourScore = 0;
+    let computerScore = 0;
+    let rounds;
+
+    while(true){
+        let input = prompt('How many rounds do you want to play? (1-10)');
+        rounds = parseInt(input, 10);
+
+        if(Number.isInteger(rounds) && rounds >= 1 && rounds <= 10){
+            break;
+        }
+        alert('Invalid input. Please enter a valid number (1-10)');
+    }
+
+    for(let i = 0; i < rounds; i++){
+        console.log('------- Round ' + (i + 1) + ' -------');
+        let yourSelection = yourChoice();
+        let computerSelection = computerChoice();
+        const roundResult = oneRound(yourSelection, computerSelection);
+
+        if(roundResult === 1){
+            yourScore++;
+        }else if(roundResult === -1){
+            computerScore++;
+        }
+        console.log('You: ' + yourScore + ' | Computer: ' + computerScore);
+    }
+
+    console.log('------- Final result -------');
+    if(yourScore > computerScore){
+        console.log('You won');
+    }else if(yourScore < computerScore){
+        console.log('You lost');
+    }else{
+        console.log('It\'s a tie');
+    }
+    
+
+}
+playGame();
